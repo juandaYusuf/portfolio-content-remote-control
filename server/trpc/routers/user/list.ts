48a1340c382cebc9727c48ceda393371b1ee8bc0
juandaYusuf <yusuf@nsi.co.id>
2025-09-13 18:01:00 +0700

    rename endpoint logic file name

---
+++ server/trpc/routers/user/list.ts
@@ -0,0 +1,24 @@
+import { TRPCError } from '@trpc/server'
+import { baseProcedure, createTRPCRouter } from '../../init'
+
+const { list } = createTRPCRouter({
+  list: baseProcedure
+  .query(async ({ ctx }) => {
+    try {
+      return await ctx.prisma.user.findMany()
+    } catch (err: any) {
+      if (err.code === 'P2025') {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: 'User not found',
+        })
+      }
+      throw new TRPCError({
+        code: 'INTERNAL_SERVER_ERROR',
+        message: 'Something went wrong',
+      })
+    }
+  }),
+})
+
+export default list
